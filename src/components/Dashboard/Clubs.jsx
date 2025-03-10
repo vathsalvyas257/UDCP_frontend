83897220236b4a71080b96c4fbb7e32a85bde7b2
PUJARI-SAIBALAJI <saibalajipujari@gmail.com>
2025-03-10 16:19:10 +0530

    clubs error solved

--- src/components/Dashboard/Clubs.jsx
+++ src/components/Dashboard/Clubs.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 import CubeLoader from "../loaders/CubeLoader"; // Import the CubeLoader component
 
 // Backend API URL
-const API_URL = `${import.meta.env.VITE_BASE_URL||""}/club`;
+const API_URL = `${import.meta.env.VITE_BASE_URL||"/api"}/club`;
 
 const Clubs = () => {
   const [clubs, setClubs] = useState([]);
